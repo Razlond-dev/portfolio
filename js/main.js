911600e6e05c26f3dfc8069d1191da253397c6cb
Nazar Pidhirnyi <nazar.pidhirnyi@gmail.com>
2021-03-15 12:58:40 +0100

    refactor: adjust main information

--- js/main.js
+++ js/main.js
@@ -9,3 +9,9 @@ ScrollOut({
   targets:
     ".box-inner.reverse .box-image ,nav, span, h2, h1, p, a, img, .name, .avatar,.box-image, .p-box, #contact-form",
 });
+
+const year = document.querySelector('#year')
+
+let currentYear = new Date
+
+year.textContent = `${currentYear.getFullYear()}`
